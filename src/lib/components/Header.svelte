92c10af2ad5275a7c18a9967b46eefec98c7d6a4
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-07-31 23:16:31 +0800

    menu header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,18 +4,11 @@
 			<div class="text-2xl font-bold text-purple-600">
 				<a href="/">Hashire!</a>
 			</div>
-			<!-- <div class="hidden md:flex items-center space-x-6">
-				<a href="#" class="text-gray-600 hover:text-purple-600 font-semibold">✧ Top</a>
-				<a href="#" class="text-gray-600 hover:text-purple-600 font-semibold">✧ News</a>
-				<a href="#" class="text-purple-600 font-bold border-b-2 border-purple-600">✧ Characters</a>
-				<a href="#" class="text-gray-600 hover:text-purple-600 font-semibold">✧ Media</a>
+			<div class="hidden md:flex items-center space-x-6">
+				<a href="#" class="text-gray-600 font-semibold hover:text-purple-600 ">✧ News</a>
+				<a href="#" class="font-bold border-b-2 text-purple-600 border-purple-600">✧ Characters</a>
 			</div>
-			<a
-				href="#"
-				class="hidden md:block bg-pink-500 text-white font-bold py-2 px-6 rounded-lg hover:bg-pink-600 transition duration-300"
-			>
-				Play Now!
-			</a>
+			
 			<div class="md:hidden">
 				<button class="text-gray-600 focus:outline-none">
 					<svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -27,7 +20,7 @@
 						></path>
 					</svg>
 				</button>
-			</div> -->
+			</div>
 		</div>
 	</div>
 </header>
