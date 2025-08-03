8688c72a6c709a977e71d5318836d23aa0a303b4
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-08-03 17:15:35 +0800

    icon size di footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -8,9 +8,9 @@
     <a href="https://trakteer.id/bagood" target="_blank"
         ><img
             id="wse-buttons-preview"
-            src="https://cdn.trakteer.id/images/embed/trbtn-red-1.png?date=18-11-2023"
-            height="40"
-            style="border: 0px; height: 40px; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;"
+            src="https://cdn.trakteer.id/images/embed/trbtn-red-1.png"
+            height="30"
+            style="border: 0px; height: 30px; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;"
             alt="Trakteer Saya"
             data-darkreader-inline-border-top=""
             data-darkreader-inline-border-right=""
